9cd84cfa3d34c43b761f8fc962282bbe00672e37
Pablo Manso <92manso@gmail.com>
2021-12-08 22:44:22 +0100

    Primera versión de la interfaz gráfica

---
+++ cryptovwap/front/main_page.py
@@ -0,0 +1,86 @@
+from .head import *
+from .menu import *
+import plotly.graph_objs as go
+
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Output, Input
+from ..back import Kraken
+
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+    {
+        "href": "https://cdnjs.cloudflare.com/ajax"
+                "/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+        "rel": "stylesheet",
+    }
+]
+
+app = dash.Dash("cryptovwap", external_stylesheets=external_stylesheets)
+app.title = "Crypto VWAP"
+server = app.server
+k = Kraken()
+
+app.layout = html.Div(
+    children=[
+        head,
+        menu(k),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="grafica-precio", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("grafica-precio", "figure"),
+    [
+        Input("filtro-crypto", "value")
+    ],
+)
+def update_charts(region):
+    k = Kraken()
+    data = k.get_data(region, extra=[k.vwap])
+
+    price_chart_figure = {
+        "data": [
+            {
+                "x": data["dtime"].apply(str),
+                "y": data["price"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+            {
+                "x": data["dtime"].apply(str),
+                "y": data["vwap"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897", "#17B8AA"],
+        },
+    }
+
+    return price_chart_figure
---
+++ cryptovwap/front/menu.py
@@ -0,0 +1,52 @@
+from .head import *
+from .menu import *
+
+import dash
+from dash import dcc
+from dash import html
+import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
+from ..back import Kraken
+from datetime import date
+
+def dropdown(id, values):
+    return html.Div(
+            children=[
+                html.Div(children="Region", className="menu-title"),
+                dcc.Dropdown(
+                    id=id,
+                    options=[
+                        {"label": x, "value": x}
+                        for x in values
+                    ],
+                    value=values[0],
+                    clearable=False,
+                    className="dropdown",
+                ),
+            ]
+        )
+
+
+def menu(k):
+    return html.Div(
+        children=[
+            dropdown("filtro-crypto", k.get_asset_pairs_symbols()),
+
+
+
+            html.Div(
+                children=[
+                    html.Div(children="Type", className="menu-title"),
+                    dcc.DatePickerSingle(
+                        id='filtro-date',
+                        min_date_allowed=date(2020, 1, 1),
+                        max_date_allowed=date.todat(),
+                        initial_visible_month=date.todat(),
+                        date=date.todat()
+                    ),
+                ],
+            ),
+        ],
+        className="menu",
+    )
